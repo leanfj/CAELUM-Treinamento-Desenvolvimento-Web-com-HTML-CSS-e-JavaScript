42abefc2d9a27268a8d7ac9cb336bfc340d28531
Leandro Ferreira de Jesus <leanfj@hotmail.com>
2017-01-29 03:05:41 -0200

    7.7 Exercícios: include Criado pagina protudo.php aplicando tecnica de INCLUDE

---
+++ produto.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <title>Mirror Fashion</title>
+
+        <link rel="stylesheet" href="css/reset.css">
+        <link rel="stylesheet" href="css/estilos.css">
+        <link rel="stylesheet" href="css/mobile.css" media="(max-width: 939px)">
+    </head>
+    <body>
+        <?php include("cabecalho.php"); ?>
+        <!--Conteudo-->
+        <?php include("rodape.php"); ?>
+    </body>
+</html>
