39ec6e6cd1658b84127aeb98f90f1308c4a0b6a5
Leandro Ferreira de Jesus <leanfj@hotmail.com>
2017-01-29 03:46:23 -0200

    7.9 Exercícios opcionais: variáveis em PHP: Juntamos toda parte inicial do site de doctype ate o header para fazer INCLUDE e a utilização de Variavel em PHP para modificar o CSS utilizado e no TITLE da pagina

--- cabecalho.php
+++ cabecalho.php
@@ -1,15 +1,30 @@
-<header class="container">
-    <h1><img src="img/logo.png" alt="Mirror Fashion"></h1>
-    <p class="sacola">
-        Nenhum item na sacola de compras
-    </p>
-    <nav class="menu-opcoes">
-        <ul>
-            <li><a href="#">Sua Conta</a></li>
-            <li><a href="#">Lista de Desejos</a></li>
-            <li><a href="#">Cartão Fidelidade</a></li>
-            <li><a href="sobre.php">Sobre</a></li>
-            <li><a href="#">Ajuda</a></li>
-        </ul>
-    </nav>
-</header>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title><?php print $cabecalho_title; ?></title>
+        <link rel="stylesheet" href="css/reset.css">
+        <link rel="stylesheet" href="css/estilos.css">
+        <?php print @$cabecalho_css; ?>
+        <link rel="stylesheet" href="css/mobile.css" media="(max-width: 939px)">
+        <meta name="viewport" content="width=device-width">
+        <!--[if lt IE 9]>
+            <script src="bower_components/html5shiv/dist/html5shiv.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <header class="container">
+            <h1><img src="img/logo.png" alt="Mirror Fashion"></h1>
+            <p class="sacola">
+                Nenhum item na sacola de compras
+            </p>
+            <nav class="menu-opcoes">
+                <ul>
+                    <li><a href="#">Sua Conta</a></li>
+                    <li><a href="#">Lista de Desejos</a></li>
+                    <li><a href="#">Cartão Fidelidade</a></li>
+                    <li><a href="sobre.php">Sobre</a></li>
+                    <li><a href="#">Ajuda</a></li>
+                </ul>
+            </nav>
+        </header>
