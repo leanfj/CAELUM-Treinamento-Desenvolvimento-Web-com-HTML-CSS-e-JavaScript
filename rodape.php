3b8a2c1750934b7b155632c19525889408fa183d
Leandro Ferreira de Jesus <leanfj@hotmail.com>
2017-01-29 03:04:55 -0200

    7.7 Exercícios: include cariado rodape.php para utilizar com função INCLUDE substituindo footer

---
+++ rodape.php
@@ -0,0 +1,16 @@
+<footer>
+    <div class="container">
+        <img src="img/logo-rodape.png" alt="Logo Mirror Fashion">
+        <ul class="social">
+            <li>
+                <a href="https://www.facebook.com/mirrorfashion">Facebook</a>
+            </li>
+            <li>
+                <a href="https://twitter.com/mirrorfashion">Twitter</a>
+            </li>
+            <li>
+                <a href="https://plus.google.com/mirrorfashion">Google+</a>
+            </li>
+        </ul>
+    </div>
+</footer>
