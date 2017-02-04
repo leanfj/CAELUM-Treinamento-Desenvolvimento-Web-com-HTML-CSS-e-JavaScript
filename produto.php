250b13e91ced4a6890f9e84289cfbb4707e8212f
Leandro Ferreira de Jesus <leanfj@hotmail.com>
2017-02-04 18:46:00 -0200

    8.9 Exercícios: responsive design - Utilização da class container para centralizar o conteudo.

--- produto.php
+++ produto.php
@@ -3,27 +3,29 @@
             $cabecalho_title = "Produto da Mirror Fashion";
             include("cabecalho.php"); ?>
         <!--Conteudo-->
-        <div class="produto">
-            <h1>Fuzzy Cardigan</h1>
-            <p>por apenas R$ 129,00</p>
-            <form action="">
-                <fieldset class="cores">
-                    <legend>Escolaha a cor:</legend>
+        <div class="container">
+            <div class="produto">
+                <h1>Fuzzy Cardigan</h1>
+                <p>por apenas R$ 129,00</p>
+                <form action="">
+                    <fieldset class="cores">
+                        <legend>Escolaha a cor:</legend>
 
-                    <input type="radio" name="cor" value="verde" id="verde" checked>
-                    <label for="verde"><img src="img/produtos/foto2-verde.png" alt="verde"></label>
+                        <input type="radio" name="cor" value="verde" id="verde" checked>
+                        <label for="verde"><img src="img/produtos/foto2-verde.png" alt="verde"></label>
 
-                    <input type="radio" name="cor" value="rosa" id="rosa">
-                    <label for="rosa">
-                    <img src="img/produtos/foto2-rosa.png" alt=""></label>
+                        <input type="radio" name="cor" value="rosa" id="rosa">
+                        <label for="rosa">
+                        <img src="img/produtos/foto2-rosa.png" alt=""></label>
 
-                    <input type="radio" name="cor" value="azul" id="azul">
-                    <label for="azul">
-                    <img src="img/produtos/foto2-azul.png" alt=""></label>
+                        <input type="radio" name="cor" value="azul" id="azul">
+                        <label for="azul">
+                        <img src="img/produtos/foto2-azul.png" alt=""></label>
 
-                </fieldset>
-                <input type="submit" class="comprar" value="comprar">
-            </form>
+                    </fieldset>
+                    <input type="submit" class="comprar" value="comprar">
+                </form>
+            </div>
         </div>
         <?php include("rodape.php"); ?>
     </body>
