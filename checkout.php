9ea70c74d74448f67796e76060d7ffdb7d63f0d4
Leandro Ferreira de Jesus <leanfj@hotmail.com>
2017-02-12 23:37:13 -0200

    11.8 Exercícios: formatando um número - Inclusão de script para conversão de de moeda formatando para string e number

--- checkout.php
+++ checkout.php
@@ -114,6 +114,7 @@
         }
       };
     </script>
+    <script src="js/converteMoeda.js"></script>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Latest compiled and minified JavaScript -->
